b5a682073b6aa49699ed707827e26043987e2dc3
bichão <yuri.souza13@fatec.sp.gov.br>
2026-03-05 23:56:42 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
+/**
+ * Alterna a visualização entre ícone e descrição nos cards de tecnologia
+ * @param {HTMLElement} element - O elemento do card clicado
+ */
+function toggleCard(element) {
+    // Seleciona todos os cards de tecnologia
+    const todosOsCards = document.querySelectorAll('.card-tech');
 
+    // Fecha os outros cards que estiverem abertos
+    todosOsCards.forEach(card => {
+        if (card !== element) {
+            card.classList.remove('active');
+        }
+    });
+
+    // Ativa ou desativa o card atual
+    element.classList.toggle('active');
+}
